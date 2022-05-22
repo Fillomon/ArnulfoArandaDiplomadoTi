d540c9136c6b3ca877b650b5057acff6cb840efe
Arnulfo Aranda Castro <102466555+Fillomon@users.noreply.github.com>
2022-05-22 01:28:48 -0500

    22mayo

---
+++ 7Semanaultima/2do/scrip.py
@@ -0,0 +1,11 @@
+import requests
+r= requests.get("https://api.github.com/search/repositories?q=language:python")
+data=r.json( )
+
+print(type(data))
+#print(data.items())
+
+for k,v in data.items():
+    print(k,'->',v,'\n')
+
+print(data['total_count'])
